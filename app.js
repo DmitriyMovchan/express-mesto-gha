76a402319776fc195adcb41a0c1741b5359081ee
Dmitriy Movchan <heavysky@yandex.ru>
2022-06-08 12:17:11 +0300

    добавлено логирование

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const { userRouter } = require('./routes/user');
 const { cardRouter } = require('./routes/card');
 const { isAuthorized } = require('./middlewares/auth');
 const { NotFoundError } = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.post('/signin', userRouter);
 app.post('/signup', userRouter);
@@ -19,6 +22,9 @@ app.use('/', userRouter);
 app.use('/cards', cardRouter);
 // eslint-disable-next-line no-shadow
 app.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемая страница не найдена')));
+
+app.use(errorLogger);
+
 app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
